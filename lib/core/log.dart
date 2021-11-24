9f103f22e36f31d66f47fcdacdc8bb17ca9f914a
yixin.panyi <panyi@yixin.im>
2021-11-24 11:39:04 +0800

    update LogUtil

--- lib/core/log.dart
+++ lib/core/log.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
 
 class LogUtil {
-  static const bool debug = true;
-
+  /// 是否为Release模式
+  static const bool isReleaseMode = bool.fromEnvironment("dart.vm.product");
 
   static void log(String? msg) {
-    if(!debug){
+    if(isReleaseMode){
       return;
     }
     
+    // ignore: avoid_print
     print(msg);
   }
   
