402aa5d61e6656f167090bcbf3793ed39a48f9a2
yixin.panyi <panyi@yixin.im>
2022-01-04 19:13:56 +0800

    add input action base class

---
+++ lib/widget/more_action.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+///
+///
+///
+class InputAction{
+  String name;
+  String icon;
+
+  InputAction(this.name , this.icon);
+
+  void onClickAction(BuildContext context){
+  }
+}
